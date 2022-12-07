9a1f1564370cf60d5b139a90cc9431fe9d8219f3
ClassicMC <classicmclive@gmail.com>
2022-12-07 10:33:58 -0500

    Update bunsesScript.java

--- bunsesScript.java
+++ bunsesScript.java
@@ -1,25 +1,23 @@
+import java.util.Scanner;
 /*
 BunsesScript, Java edition,
 developed by, ClassicMC.
 credits(); for more info.
 */
 
-public class bunsesScript {
+public class Main {
     public static void main(String[] args) {
       usingBunses();
       //Main code
-      
     }
-    static void usingBunses()
-    {
+    static void usingBunses(){
         //Imports bunses library
+        System.out.println("/Using bunsesScript 1.0.1\\");
     }
-    static void printf(String bunses)
-    {
+    static void printf(String bunses){
         System.out.print(bunses);
     }
-    static void loop(String text,Boolean x, int start, int length)
-    {
+    static void loop(String text,Boolean x, int start, int length){
         while(x){
             start +=1;
             printf(text);
@@ -28,19 +26,22 @@ public class bunsesScript {
             }
         }
     }
-    static void credits()
-    {
+    static void credits(){
         printf("\nMade by, ClassicMC");
-        printf("\nv0.0.1 java edition");
+        printf("\nv1.0.1 java edition");
         printf("\nhelpf() for more.");
         printf("\nSemicolons are required,");
         printf("\nBunses is based on \\\"prototype e-sharp\\\" for game delopment.");
     }
-    static void helpf()
-    {
+    static void helpf(){
         printf("\nto print items print(\"//print stuff\");");
         printf("\nto loop loop(\"//loop text\",true,//startingAmount,//endingAmount\")");
         printf("\n\\n for a new line, \\\", and \\\', \\\\ for avoding those symbols in strings");
+        printf("\nto get user input inputf();");
         printf("\nIf you are experincing errors,double check that your code has semicolons \";\"");
+    }
+    static String inputf(){
+        Scanner s1 = new Scanner(System.in);
+        return s1.nextLine();
     }
 }
